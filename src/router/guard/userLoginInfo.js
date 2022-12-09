76c87e8a94704b20199baa1e38c63623e928162c
ouyangshun <ouyangshun@hxdi.com>
2022-12-09 08:49:49 +0800

    fix: 修复403页面返回首页重定向

--- src/router/guard/userLoginInfo.js
+++ src/router/guard/userLoginInfo.js
@@ -27,12 +27,14 @@ export default function setupUserLoginInfoGuard(router) {
         }
       }
     } else {
-      const hasPermissions = Boolean(to?.meta?.permissions);
-      const requiresAuth = Boolean(to?.meta?.requiresAuth ?? true);
+      // to 的 meta 是一个非递归合并所有 meta 字段的（从父字段到子字段），所以只能从 matched 取最后一个
+      const toRouter = to.matched[to.matched.length - 1];
+      const hasPermissions = Boolean(toRouter?.meta?.permissions);
+      const requiresAuth = Boolean(toRouter?.meta?.requiresAuth ?? true);
       // 没有登陆的情况下，如果当前页面在登陆页面或者当前页面不需要权限
-      if (to.name === "login" || !requiresAuth) {
+      if (toRouter.name === "login" || !requiresAuth) {
         if (!hasPermissions && !requiresAuth) {
-          console.warn(`[Castle] 页面 ${to.name} 路由信息里面 meta -> permissions 未配置`);
+          console.warn(`[Castle] 页面 ${toRouter.name} 路由信息里面 meta -> permissions 未配置`);
         }
 
         next();
