53dc68ccd6ba63592180b0bcbe1d75bfbf6e90b5
ouyangshun <ouyangshun@hxdi.com>
2023-02-15 09:43:12 +0800

    feat: 添加生命周期配置

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import ProLayout, { PageContainer } from "@ant-design-vue/pro-layout";
 import registerIcons from "#/utils/register-icons.js";
 import provideUtils from "#/utils/provide-utils.js";
 import PermissionDirective from "#/directive/permission";
+import userSettings from "@/config/settings.js";
 
 import "ant-design-vue/dist/antd.css";
 import "@ant-design-vue/pro-layout/dist/style.css";
@@ -17,6 +18,10 @@ import "#/utils/request-interceptor";
 
 const app = createApp(App);
 
+if (typeof userSettings?.lifecycle?.beforeMount === "function") {
+  userSettings?.lifecycle?.beforeMount(app);
+}
+
 app.use(createPinia());
 app.use(router);
 app.use(Antd);
@@ -28,5 +33,9 @@ registerIcons(app);
 provideUtils(app);
 
 app.mount("#app");
+
+if (typeof userSettings?.lifecycle?.mounted === "function") {
+  userSettings?.lifecycle?.mounted(app);
+}
 
 export default app;
