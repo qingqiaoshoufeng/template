813e754930c9f6ca8b1ce73b57c190b2896f9e29
ouyangshun <ouyangshun@hxdi.com>
2023-04-18 10:06:17 +0800

    fix: fix bug

--- src/layout/basic-layout.vue
+++ src/layout/basic-layout.vue
@@ -23,7 +23,7 @@
 
     <template #rightContentRender>
       <a-space :size="10">
-        <RenderJsxComponents v-for="c in userSettings?.userNavigationComponents" :key="c" :components="c" />
+        <RenderJsxComponents v-for="c in userSettings?.userNavigationComponents" :key="c" :componentVnode="c" />
         <template v-if="userSettings?.darkness?.showSwitch">
           <a-divider type="vertical" style="background-color: #6f8edd" />
           <darkness-mode-switch />
